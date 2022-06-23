6e0a7b6dbb096a97d85411d7d761e81702e12af2
vaniorodrigues <rodriguesvanio@gmail.com>
2022-06-23 13:53:30 -0300

    Favorite button add plus some code refactoring.

--- lib/pages/universities_list_page.dart
+++ lib/pages/universities_list_page.dart
@@ -10,119 +10,67 @@ import '../widgets/circular_progress.dart';
 import '../models/country.dart';
 import 'university_info_page.dart';
 
-class UniversitiesListPage extends StatefulWidget {
+class UniversitiesListPage extends StatelessWidget {
   final List<Country> countries;
   final int index;
 
-  const UniversitiesListPage(
-      {Key? key, required this.index, required this.countries})
+  UniversitiesListPage({Key? key, required this.index, required this.countries})
       : super(key: key);
 
-  @override
-  State<UniversitiesListPage> createState() => _UniversitiesListPageState();
-}
-
-// TODO: Add a search field to filter the universities by name.
-// TODO: Only one body is needed, make the API body a call to a function that saves the university data in the database, the database is then loaded.
-
-class _UniversitiesListPageState extends State<UniversitiesListPage> {
   final TransactionWebClient _webClient = TransactionWebClient();
 
   final UniversityDao _daoUni = UniversityDao();
   final CountryDao _daoCountry = CountryDao();
+
   @override
   Widget build(BuildContext context) {
-    Country selectedCountry = widget.countries[widget.index];
-    _daoUni.findAll();
-
-    debugPrint(
-        'Is local data available: ${selectedCountry.isLocalDataAvailable}');
+    Country selectedCountry = countries[index];
     return Scaffold(
       appBar: AppBar(
         title: Text('Universities in ${selectedCountry.name}'),
         backgroundColor: Theme.of(context).primaryColor,
       ),
       // Tests if the country has already been searched in the web api, if so, shows the database data.
-      body: selectedCountry.isLocalDataAvailable == 1
-          ? _GetUniversitiesFromDB(
-              dao: _daoUni,
-              widget: widget,
-              countries: widget.countries,
-            )
-          : _GetUniversitiesFromAPI(
-              webClient: _webClient,
-              widget: widget,
-              countries: widget.countries,
-              daoUni: _daoUni,
-              daoCountry: _daoCountry,
-            ),
+      body: _GetUniversities(
+        webClient: _webClient,
+        countries: countries,
+        daoUni: _daoUni,
+        daoCountry: _daoCountry,
+        index: index,
+      ),
     );
   }
 }
 
-class _GetUniversitiesFromDB extends StatelessWidget {
-  const _GetUniversitiesFromDB({
-    Key? key,
-    required UniversityDao dao,
-    required this.widget,
-    required this.countries,
-  })  : _dao = dao,
-        super(key: key);
-
-  final UniversityDao _dao;
-  final UniversitiesListPage widget;
-  final List<Country> countries;
-
-  @override
-  Widget build(BuildContext context) {
-    debugPrint('getting from DB');
-    return FutureBuilder<List<University>>(
-        initialData: const [],
-        future: _dao.findByCountry(countries[widget.index].name),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              break;
-            case ConnectionState.waiting:
-              return const Progress();
-            case ConnectionState.active:
-              break;
-            case ConnectionState.done:
-              final List<University> universities = snapshot.data!;
-              return _UniversityListViewBuilder(
-                universities: universities,
-                daoUni: _dao,
-              );
-          }
-          return const Text('Error in loading the database data');
-        });
-  }
-}
-
-class _GetUniversitiesFromAPI extends StatelessWidget {
+class _GetUniversities extends StatefulWidget {
   final TransactionWebClient _webClient;
-  final UniversitiesListPage widget;
   final List<Country> countries;
   final UniversityDao _daoUni;
   final CountryDao _daoCountry;
+  final int index;
 
-  const _GetUniversitiesFromAPI({
+  const _GetUniversities({
     Key? key,
     required TransactionWebClient webClient,
-    required this.widget,
     required this.countries,
     required UniversityDao daoUni,
     required CountryDao daoCountry,
+    required this.index,
   })  : _webClient = webClient,
         _daoUni = daoUni,
         _daoCountry = daoCountry,
         super(key: key);
 
   @override
+  State<_GetUniversities> createState() => _GetUniversitiesState();
+}
+
+class _GetUniversitiesState extends State<_GetUniversities> {
+  @override
   Widget build(BuildContext context) {
-    debugPrint('getting from API');
     return FutureBuilder<List<University>>(
-      future: _webClient.findByCountry(countries[widget.index].name),
+      initialData: const [],
+      future: _loadsUniversities(widget.countries[widget.index]),
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
@@ -133,20 +81,21 @@ class _GetUniversitiesFromAPI extends StatelessWidget {
             break;
           case ConnectionState.done:
             if (snapshot.hasData) {
-              // Saves the data in the universities data from snapshtot into the database UniversityDao
               final List<University> universities = snapshot.data!;
-              _saveUniversitiesDao(universities);
-
-              // Updates the country's isLocalDataAvailable to 1 to indicate that the data is available in the database, also updates the number of universities in the country.
-              countries[widget.index].foundUniversities = universities.length;
-              countries[widget.index].isLocalDataAvailable = 1;
-              _daoCountry.updateCountry(countries[widget.index]);
-              // _daoCountry.save(countries[widget.index]);
-
               if (universities.isNotEmpty) {
                 return _UniversityListViewBuilder(
                   universities: universities,
-                  daoUni: _daoUni,
+                  daoUni: widget._daoUni,
+                  onClick: (university) {
+                    Navigator.of(context)
+                        .push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                UniversityInfoPage(university),
+                          ),
+                        )
+                        .then((value) => setState(() {}));
+                  },
                 );
               }
               return const CenteredMessage(
@@ -156,14 +105,29 @@ class _GetUniversitiesFromAPI extends StatelessWidget {
                 message: 'No data found!', icon: Icons.warning);
         }
         return const CenteredMessage(
-            message: 'No data found 2!!', icon: Icons.warning);
+            message: 'Unknown Error', icon: Icons.warning);
       },
     );
   }
 
+  Future<List<University>> _loadsUniversities(Country country) async {
+    if (country.isLocalDataAvailable == 0) {
+      final List<University> universities =
+          await widget._webClient.findByCountry(country.name);
+      await _saveUniversitiesDao(universities);
+      country.foundUniversities = universities.length;
+      country.isLocalDataAvailable = 1;
+      await widget._daoCountry.updateCountry(country);
+      return universities;
+    }
+    List<University> universities =
+        await widget._daoUni.findByCountry(country.name);
+    return universities;
+  }
+
   Future<void> _saveUniversitiesDao(List<University> universities) async {
     for (final university in universities) {
-      await _daoUni.save(university);
+      await widget._daoUni.save(university);
     }
   }
 }
@@ -173,28 +137,26 @@ class _UniversityListViewBuilder extends StatelessWidget {
     Key? key,
     required this.universities,
     required this.daoUni,
+    required this.onClick,
   }) : super(key: key);
 
   final List<University> universities;
   final UniversityDao daoUni;
+  final Function onClick;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
+      separatorBuilder: ((context, index) => const Divider()),
       itemCount: universities.length,
+      padding: const EdgeInsets.all(16),
       itemBuilder: (context, index) {
         final University university = universities[index];
-        debugPrint(
-            '_UniversityListViewBuilder --> university: ${university.toString()}');
         return Card(
           child: ListTile(
             trailing: FavoriteButton(university: university, daoUni: daoUni),
             onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => UniversityInfoPage(university),
-                ),
-              );
+              onClick(university);
             },
             title: Text(
               university.name,
